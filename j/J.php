5b119316fe511c67b8a82f964ef6c9b76d9bf99a
worawich-2004 <66010914036@msu.ac.th>
2026-02-18 15:17:57 +0700

    aa

---
+++ j/J.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010914036วรวิช โนนแดง(อ๊อฟ)</title>
+</head>
+
+<body>
+<br>
+<h1>งาน--j66010914036วรวิช โนนแดง(อ๊อฟ)</h1>
+<img src="images/1.jpg"width="500">
+<img src="images/2.jpg"width="500">
+</body>
+</html>
